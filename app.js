220aca1c87ebd00b0fa943634eae1f7f290a0d3e
Aaron Ansah <ansahe663@gmail.com>
2024-05-24 13:21:19 +0000

    update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.set('trust proxy', 1);
 
 app.use(rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 }))
 
 app.use(express.json());
